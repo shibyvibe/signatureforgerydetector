006fa88bd00ef0f16b94de99c67bd6b48909e462
shibyvibe <74696071+shibyvibe@users.noreply.github.com>
2021-09-06 18:15:49 -0700

    Add files via upload

---
+++ curl_test/forgery.bash
@@ -0,0 +1,1 @@
+curl -i -X POST -H "Content-Type: multipart/form-data" -F "personId=49" -F "file=@01_0114049.PNG" http://localhost:8000/upload
---
+++ curl_test/good.sh
@@ -0,0 +1,1 @@
+curl -i -X POST -H "Content-Type: multipart/form-data" -F "personId=49" -F "file=@01_049.png" http://localhost:8000/upload
